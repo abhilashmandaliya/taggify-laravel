daeb75d310a1923faa14af00f6c5a904b7fe1ca9
abhilashmandaliya <abhilashmandaliya@gmail.com>
2018-01-28 12:57:48 +0530

    configured mongo on windows

--- config/database.php
+++ config/database.php
@@ -80,11 +80,11 @@ return [
 
         'mongodb' => [
             'driver'   => 'mongodb',
-            'host'     => env('DB_HOST', 'localhost'),
-            'port'     => env('DB_PORT', 27017),
-            'database' => env('DB_DATABASE_MONGO'),
-            'username' => env('DB_USERNAME_MONGO'),
-            'password' => env('DB_PASSWORD_MONGO'),
+            'host'     => env('DB_HOST_MONGO', 'localhost'),
+            'port'     => env('DB_PORT_MONGO', 27017),
+            'database' => env('DB_DATABASE_MONGO', null),
+            'username' => env('DB_USERNAME_MONGO', null),
+            'password' => env('DB_PASSWORD_MONGO', null),
             'options'  => [
                 'database' => 'admin' // sets the authentication database required by mongo 3
             ]
