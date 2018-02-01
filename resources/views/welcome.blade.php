2f422ed4d89151237953946753d6444de471f89d
abhilashmandaliya <abhilashmandaliya@gmail.com>
2018-02-01 11:13:13 +0530

    updated homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,10 +2,22 @@
 
 @section('content')
     <div class="flex-center position-ref full-height">
-            <div class="content-home-center">
-                    <div class="title-home-page m-b-md">
-                            <a href="/taggify-laravel/public"> #Taggify </a>
-                    </div>
-            </div>
+        <div class="widget center">
+                <div class="blur"></div>
+                <div class="text center">
+                        <h1>
+                                <a class="title-home-page" href="/taggify-laravel/public"> 
+                                        <span style="color:#fff"> #Taggify </span> 
+                                </a>
+                        </h1>
+                </div>
+        </div>
     </div>
+
+        <style>
+                body {
+                        background: url('/images/bg.png') no-repeat center center fixed;
+                        background-size: cover;  
+                }
+        </style>
 @endsection
